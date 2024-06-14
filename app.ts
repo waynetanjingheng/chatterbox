c070ae4b03bf18ed0409a15749f199d75476c275
waynetanjingheng <wtjh00@gmail.com>
2024-06-15 01:09:10 +0800

    chore: add cookies

--- app.ts
+++ app.ts
@@ -1,19 +1,22 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import * as routes from "./routes/index";
 import * as errorHandlers from "./middleware/errorhandlers";
 import * as logging from "./middleware/logging";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cookieParser());
 app.use(logging.logger);
 
 app.get("/", routes.index);
 app.get("/login", routes.login);
 app.post("/login", routes.loginProcess);
 app.get("/chat", routes.chat);
+app.get("/error", (req, res, next) => next(new Error("A Contrived Error!")));
 
 app.use(errorHandlers.error);
 app.use(errorHandlers.notFound);
